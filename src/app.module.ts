ac609ffdaff7c416c78f9ac0c162f1f966161f1c
d02ev <vikramadityapratapsingh98@gmail.com>
2023-03-08 14:37:34 +0530

    update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DatabaseModule } from './core/database/database.module';
+import { UserModule } from './module/user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    DatabaseModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.DB_URI),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
